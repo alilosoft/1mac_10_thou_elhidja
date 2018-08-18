eb080a096c5ade4ecf940078a5e23ee9c13d7d3c
@lilo$oft <fellahi.ali@gmail.com>
2018-08-18 20:57:01 +0100

    first commit

---
+++ core/src/main/java/org/cleancode/android/ten_thou_elhidja/core/MainActivity.java
@@ -0,0 +1,13 @@
+package org.cleancode.android.ten_thou_elhidja.core;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
